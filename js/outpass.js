6e5e82ad9addb9276af97c8e90070dcc6f46ab85
Thomas Saju Koshy <tomsajuk@gmail.com>
2018-02-04 21:08:36 +0530

    Add files via upload

--- js/outpass.js
+++ js/outpass.js
@@ -1,1 +1,17 @@
-document.getElementById('datepick').value = new Date().toDateInputValue();
+var date = new Date();
+
+
+
+var ddd = document.getElementsByName('da');
+var day = date.getDate();
+var month = date.getMonth() + 1;
+var year = date.getFullYear();
+
+if (month < 10) month = "0" + month;
+if (day < 10) day = "0" + day;
+
+var today = year + "-" + month + "-" + day;     
+function changetoday() {
+	document.getElementsByName("da").value = today;
+	document.getElementsByName('date').defaultvalue = "2018-09-22";
+}
